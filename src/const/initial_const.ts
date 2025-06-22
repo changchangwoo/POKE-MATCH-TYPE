8335bb170911f64e12c32e134d74c01cda119337
changchangwoo <changchangwoo@naver.com>
2025-06-22 18:57:14 +0900

    Refactor : 초기데이터 피카츄로 변경

--- src/const/initial_const.ts
+++ src/const/initial_const.ts
@@ -1,12 +1,11 @@
 import { MatchInfo } from "../models/pokemonData";
 
 export const DEFAULT_MATCH_DATA: MatchInfo = {
-  name: "리자몽",
-  no: 6,
+  name: "피카츄",
+  no: 25,
   searchLanguage: "kor",
   types: [
-    { no: 10, name: "fire" },
-    { no: 3, name: "flying" },
+    { no: 13, name: "electric" }
   ],
-  imgs: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+  imgs: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
 };
